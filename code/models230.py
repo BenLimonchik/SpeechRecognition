7d06b099c5af930672349dc3101c122c43d0a259
benlimon@stanford.edu <benlimon@stanford.edu>
2018-02-24 18:51:01 -0800

    First commit

---
+++ code/models230.py
@@ -0,0 +1,137 @@
+"""Model definitions for simple speech recognition.
+
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+
+import tensorflow as tf
+
+#Use model settings from open source code.
+def prepare_model_settings(label_count, sample_rate, clip_duration_ms,
+                           window_size_ms, window_stride_ms,
+                           dct_coefficient_count):
+  """Calculates common settings needed for all models.
+
+  Args:
+    label_count: How many classes are to be recognized.
+    sample_rate: Number of audio samples per second.
+    clip_duration_ms: Length of each audio clip to be analyzed.
+    window_size_ms: Duration of frequency analysis window.
+    window_stride_ms: How far to move in time between frequency windows.
+    dct_coefficient_count: Number of frequency bins to use for analysis.
+
+  Returns:
+    Dictionary containing common settings.
+  """
+  desired_samples = int(sample_rate * clip_duration_ms / 1000)
+  window_size_samples = int(sample_rate * window_size_ms / 1000)
+  window_stride_samples = int(sample_rate * window_stride_ms / 1000)
+  length_minus_window = (desired_samples - window_size_samples)
+  if length_minus_window < 0:
+    spectrogram_length = 0
+  else:
+    spectrogram_length = 1 + int(length_minus_window / window_stride_samples)
+  fingerprint_size = dct_coefficient_count * spectrogram_length
+  return {
+      'desired_samples': desired_samples,
+      'window_size_samples': window_size_samples,
+      'window_stride_samples': window_stride_samples,
+      'spectrogram_length': spectrogram_length,
+      'dct_coefficient_count': dct_coefficient_count,
+      'fingerprint_size': fingerprint_size,
+      'label_count': label_count,
+      'sample_rate': sample_rate,
+  }
+
+#create a model, modified from open source code to create our single fc model
+def create_model(fingerprint_input, model_settings, model_architecture,
+                 is_training, runtime_settings=None):
+  """Builds a model of the requested architecture compatible with the settings.
+
+  There are many possible ways of deriving predictions from a spectrogram
+  input, so this function provides an abstract interface for creating different
+  kinds of models in a black-box way. You need to pass in a TensorFlow node as
+  the 'fingerprint' input, and this should output a batch of 1D features that
+  describe the audio. Typically this will be derived from a spectrogram that's
+  been run through an MFCC, but in theory it can be any feature vector of the
+  size specified in model_settings['fingerprint_size'].
+
+  The function will build the graph it needs in the current TensorFlow graph,
+  and return the tensorflow output that will contain the 'logits' input to the
+  softmax prediction process. If training flag is on, it will also return a
+  placeholder node that can be used to control the dropout amount.
+
+  See the implementations below for the possible model architectures that can be
+  requested.
+
+  Args:
+    fingerprint_input: TensorFlow node that will output audio feature vectors.
+    model_settings: Dictionary of information about the model.
+    model_architecture: String specifying which kind of model to create.
+    is_training: Whether the model is going to be used for training.
+    runtime_settings: Dictionary of information about the runtime.
+
+  Returns:
+    TensorFlow node outputting logits results, and optionally a dropout
+    placeholder.
+
+  Raises:
+    Exception: If the architecture type isn't recognized.
+  """
+  if model_architecture == 'single_fc':
+    return create_single_fc_model(fingerprint_input, model_settings,
+                                  is_training)
+
+
+def load_variables_from_checkpoint(sess, start_checkpoint):
+  """Utility function to centralize checkpoint restoration.
+
+  Args:
+    sess: TensorFlow session.
+    start_checkpoint: Path to saved checkpoint on disk.
+  """
+  saver = tf.train.Saver(tf.global_variables())
+  saver.restore(sess, start_checkpoint)
+
+
+#create the single fully connected layer. modified from an open source model.
+def create_single_fc_model(fingerprint_input, model_settings, is_training):
+  """Builds a model with a single hidden fully-connected layer.
+
+  This is a very simple model with just one matmul and bias layer. As you'd
+  expect, it doesn't produce very accurate results, but it is very fast and
+  simple, so it's useful for sanity testing.
+
+  Here's the layout of the graph:
+
+  (fingerprint_input)
+          v
+      [MatMul]<-(weights)
+          v
+      [BiasAdd]<-(bias)
+          v
+
+  Args:
+    fingerprint_input: TensorFlow node that will output audio feature vectors.
+    model_settings: Dictionary of information about the model.
+    is_training: Whether the model is going to be used for training.
+
+  Returns:
+    TensorFlow node outputting logits results, and optionally a dropout
+    placeholder.
+  """
+  if is_training:
+    dropout_prob = tf.placeholder(tf.float32, name='dropout_prob')
+  fingerprint_size = model_settings['fingerprint_size']
+  label_count = model_settings['label_count']
+  weights = tf.Variable(
+      tf.truncated_normal([fingerprint_size, label_count], stddev=0.001))
+  bias = tf.Variable(tf.zeros([label_count]))
+  logits = tf.matmul(fingerprint_input, weights) + bias
+  if is_training:
+    return logits, dropout_prob
+  else:
+    return logits
